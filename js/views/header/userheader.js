427d5e6bd41cea79f8f183596c8589ce72cd72f3
Mazo <john.viklund@gmail.com>
2013-08-19 10:12:05 +0200

    Logout now triggers a redirect on the server for safer user data

--- js/views/header/userheader.js
+++ js/views/header/userheader.js
@@ -1,19 +1,8 @@
-define([ 'jquery', 'underscore', 'backbone', 'events', 'text!templates/header/userheader.html' ], function($, _, Backbone, Events, userHeaderTemplate) {
+define([ 'jquery', 'underscore', 'backbone', 'text!templates/header/userheader.html' ], function($, _, Backbone, userHeaderTemplate) {
   var UserHeaderView = Backbone.View.extend({
     el : '#header',
     render : function() {
       $(this.el).html(userHeaderTemplate);
-    },
-    events : {
-      'click #logout' : 'logout'
-    },
-    logout : function(e) {
-      $.ajax('/logout', {
-        type : 'GET',
-        success : function() {
-          Events.trigger('logout');
-        }
-      });
     }
   });
   return UserHeaderView;
