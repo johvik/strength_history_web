bbaa75ae4de8b6cda537df48ab060e8c829579dd
Mazo <john.viklund@gmail.com>
2013-09-14 22:23:25 +0200

    Started work on signup requests to the server

--- js/views/signup/page.js
+++ js/views/signup/page.js
@@ -9,6 +9,46 @@ define([
     el : '#page',
     render : function() {
       $(this.el).html(signupPageTemplate);
+    },
+    events : {
+      'click button[type="submit"]' : 'onSignUp'
+    },
+    onSignUp : function(e) {
+      e.preventDefault();
+      if (this.$('button[type="submit"]').hasClass('disabled')) {
+        return; // Prevent multiple clicks
+      }
+      this.$('button[type="submit"]').button('loading');
+      var email = this.$('#email').val();
+      var password = this.$('#password').val();
+      var passwordRepeat = this.$('#password-repeat').val();
+      var emailPattern = new RegExp('^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$');
+      if (!email || !emailPattern.test(email) || !password || password.length < 7 || password !== passwordRepeat) {
+        // Bad input
+        // TODO
+        console.log('bad input');
+        this.$('button[type="submit"]').button('reset');
+      } else {
+        var _self = this;
+        $.ajax('/signup', {
+          type : 'POST',
+          data : {
+            email : email,
+            password : password
+          },
+          error : function(jqXHR) {
+            // TODO
+            if (jqXHR.status === 409) {
+              // email in use
+            }
+            console.log('sign up failed');
+            _self.$('button[type="submit"]').button('reset');
+          },
+          success : function() {
+            // TODO Do something!
+          }
+        });
+      }
     }
   });
   return SignupPage;
